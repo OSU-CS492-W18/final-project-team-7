3378f2161f18c087888115ac125767855834b2b6
DrakeSeifert <drakeseifert1@gmail.com>
2018-02-28 18:48:55 -0800

    Created app skeleton

--- RateCatz/app/src/main/java/com/example/drake/ratecatz/MainActivity.java
+++ RateCatz/app/src/main/java/com/example/drake/ratecatz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.drake.ratecatz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        final Intent favoritesActivityIntent = new Intent(this, FavoriteCatzActivity.class);
+        final Intent settingsActivityIntent = new Intent(this, SettingsActivity.class);
+
+        switch (item.getItemId()) {
+            case R.id.action_favorites:
+                startActivity(favoritesActivityIntent);
+                return true;
+            case R.id.action_settings:
+                startActivity(settingsActivityIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
