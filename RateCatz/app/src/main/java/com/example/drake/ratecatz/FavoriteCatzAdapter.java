8e167ff63bc4a547b3bd39228f911a4285628e71
Alea Weeks <aleaweeks@10-248-112-82.wireless.oregonstate.edu>
2018-03-15 16:32:53 -0700

    Replaced async task loader with glide. Added onClickListeners to cat photos

--- RateCatz/app/src/main/java/com/example/drake/ratecatz/FavoriteCatzAdapter.java
+++ RateCatz/app/src/main/java/com/example/drake/ratecatz/FavoriteCatzAdapter.java
@@ -1,67 +1,31 @@
 package com.example.drake.ratecatz;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import com.bumptech.glide.Glide;
 import com.example.drake.ratecatz.utils.CatUtils;
 
 /**
  * Created by aleaweeks on 3/12/18.
  */
 
-public class FavoriteCatzAdapter extends FragmentStatePagerAdapter {
-
-    CatUtils.CatPhoto[] mCatPhotos;
-
-    public FavoriteCatzAdapter(FragmentManager fm) {
-        super(fm);
-    }
-
-    public void updatePhotos(CatUtils.CatPhoto[] catPhotos) {
-        mCatPhotos = catPhotos;
-        notifyDataSetChanged();
-    }
+public class FavoriteCatzAdapter  extends RecyclerView.Adapter {
 
     @Override
-    public Fragment getItem(int position) {
-        Fragment fragment = new CatPhotoFragment();
-        Bundle args = new Bundle();
-        args.putString(CatPhotoFragment.ARGS_PHOTO_URL, mCatPhotos[position].url);
-        fragment.setArguments(args);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
     }
 
     @Override
-    public int getCount() {
-        if(mCatPhotos != null){
-            return mCatPhotos.length;
-        } else {
-            return 0;
-        }
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
     }
 
-    public static class CatPhotoFragment extends Fragment {
-        public static final String ARGS_PHOTO_URL = "catUrl";
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.cat_photo_pager_item, container, false);
-            Bundle args = getArguments();
-            ImageView photoIV = (ImageView)rootView.findViewById(R.id.iv_photo);
-            String photoUrl = args.getString(ARGS_PHOTO_URL);
-            Glide.with(photoIV.getContext())
-                    .load(photoUrl)
-                    .into(photoIV);
-            return rootView;
-        }
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
--- RateCatz/app/src/main/java/com/example/drake/ratecatz/MainActivity.java
+++ RateCatz/app/src/main/java/com/example/drake/ratecatz/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.drake.ratecatz;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,19 +14,26 @@ import com.example.drake.ratecatz.utils.NetworkUtils;
 
 import org.xml.sax.SAXException;
 import com.bumptech.glide.Glide;
+
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements android.support.v4.app.LoaderManager.LoaderCallbacks<String>{
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int CAT_LOADER_ID = 1;
     private static final String CAT_URL_KEY = "catUrl";
     private static final int NUM_PHOTO_COLUMNS = 2;
+    private ArrayList<CatUtils.CatPhoto> mCatPhotos;
     private ImageView mCatPhotoOneImageView;
     private ImageView mCatPhotoTwoImageView;
+    private ProgressBar mLoadingProgressBar;
+    private TextView mLoadingErrorMessage;
 
 
     @Override
@@ -33,12 +41,36 @@ public class MainActivity extends AppCompatActivity implements android.support.v
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportLoaderManager().initLoader(CAT_LOADER_ID, null, this);
+        mLoadingProgressBar = (ProgressBar)findViewById(R.id.pb_loading_indicator);
+        mLoadingErrorMessage = (TextView)findViewById(R.id.tv_loading_error_message);
+
+        mCatPhotoOneImageView = (ImageView)findViewById(R.id.iv_cat_photo_one);
+        mCatPhotoTwoImageView = (ImageView)findViewById(R.id.iv_cat_photo_two);
+
+        mCatPhotoOneImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCatPhotoClicked();
+            }
+        });
+
+        mCatPhotoTwoImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCatPhotoClicked();
+            }
+        });
+
         doCatGetImageRequest();
 
-          mCatPhotoOneImageView = (ImageView)findViewById(R.id.iv_cat_photo_one);
-          mCatPhotoTwoImageView = (ImageView)findViewById(R.id.iv_cat_photo_two);
 
+
+    }
+
+    public void onCatPhotoClicked() {
+        String catImageUrl = CatUtils.buildGetCatImagesURL();
+        Log.d(TAG, "doCatImageRequest building another URL: " + catImageUrl);
+        new CatImageFetchTask().execute(catImageUrl);
     }
 
     @Override
@@ -65,94 +97,66 @@ public class MainActivity extends AppCompatActivity implements android.support.v
         }
     }
 
-
     private void doCatGetImageRequest() {
         String catImageUrl = CatUtils.buildGetCatImagesURL();
         Log.d(TAG, "doCatImageRequest building URL: " + catImageUrl);
-
-        Bundle argsBundle = new Bundle();
-        argsBundle.putString(CAT_URL_KEY, catImageUrl);
-        getSupportLoaderManager().restartLoader(CAT_LOADER_ID, argsBundle, this);
-
+        new CatImageFetchTask().execute(catImageUrl);
     }
 
-    @SuppressLint("StaticFieldLeak")
-    @Override
-    public android.support.v4.content.Loader<String> onCreateLoader(int id, final Bundle args) {
-        return new android.support.v4.content.AsyncTaskLoader<String>(this) {
+    public class CatImageFetchTask extends AsyncTask<String, Void, String> {
 
-            String mCatResultsXML;
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            mLoadingProgressBar.setVisibility(View.VISIBLE);
+        }
 
-            @Override
-            protected void onStartLoading() {
-                if(args != null) {
-                    if (mCatResultsXML != null) {
-                        deliverResult(mCatResultsXML);
-                    } else {
-                        forceLoad();
+        @Override
+        protected String doInBackground(String... strings) {
+            String catRequestURL = strings[0];
+
+            String catResults = null;
+
+            try{
+                catResults = NetworkUtils.doHTTPGet(catRequestURL);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            return catResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            mLoadingProgressBar.setVisibility(View.INVISIBLE);
+            if(s != null) {
+                try {
+                    mCatPhotos = CatUtils.parseCatAPIGetImageResultXML(s);
+                    CatUtils.CatPhoto catPhoto1 = new CatUtils.CatPhoto();
+                    CatUtils.CatPhoto catPhoto2 = new CatUtils.CatPhoto();
+
+                    catPhoto1 = mCatPhotos.get(0);
+                    catPhoto2 = mCatPhotos.get(1);
+
+                    Glide.with(mCatPhotoOneImageView.getContext())
+                    .load(catPhoto1.url)
+                    .into(mCatPhotoOneImageView);
+
+                    Glide.with(mCatPhotoTwoImageView.getContext())
+                            .load(catPhoto2.url)
+                            .into(mCatPhotoTwoImageView);
+
+                    for(CatUtils.CatPhoto photo : mCatPhotos) {
+                        Log.d(TAG, "Got photo" + photo.url);
                     }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
                 }
 
+            } else {
+                mLoadingErrorMessage.setVisibility(View.VISIBLE);
             }
-            @Override
-            public String loadInBackground() {
-                 if(args != null) {
-                     String catUrl = args.getString(CAT_URL_KEY);
-                     Log.d(TAG, "AsyncTaskLoader making network call: " + catUrl);
-                     String catResult = null;
-                     try {
-                         catResult = NetworkUtils.doHTTPGet(catUrl);
-                     } catch (IOException e) {
-                         e.printStackTrace();
-                     }
-                     return catResult;
-                 } else {
-                     return null;
-                 }
-            }
-
-            @Override
-            public void deliverResult(String data) {
-                mCatResultsXML = data;
-                super.deliverResult(data);
-            }
-        };
-    }
-
-    @Override
-    public void onLoadFinished(android.support.v4.content.Loader<String> loader, String data) {
-        Log.d(TAG, "AsyncTaskLoader's onLoadFinished called");
-        if(data != null) {
-            try {
-                ArrayList<CatUtils.CatPhoto> catPhotoList = new ArrayList<CatUtils.CatPhoto>();
-                catPhotoList = CatUtils.parseCatAPIGetImageResultXML(data);
-
-                CatUtils.CatPhoto catPhoto1 = new CatUtils.CatPhoto();
-                CatUtils.CatPhoto catPhoto2 = new CatUtils.CatPhoto();
-
-                catPhoto1 = catPhotoList.get(0);
-                catPhoto2 = catPhotoList.get(1);
-
-                Glide.with(mCatPhotoOneImageView.getContext())
-                .load(catPhoto1.url)
-                .into(mCatPhotoOneImageView);
-
-                Glide.with(mCatPhotoTwoImageView.getContext())
-                        .load(catPhoto2.url)
-                        .into(mCatPhotoTwoImageView);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (SAXException e) {
-                e.printStackTrace();
-            }
-        } else {
-
         }
-    }
-
-    @Override
-    public void onLoaderReset(android.support.v4.content.Loader<String> loader) {
-        // nothing to do ...
     }
 }
